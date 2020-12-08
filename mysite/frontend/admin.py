071ca15b4df2fc2fd28ba141d5547f13017f3f65
AnishaDiyya2000 <65268300+AnishaDiyya2000@users.noreply.github.com>
2020-12-08 23:23:07 +0530

    Add files via upload

---
+++ mysite/frontend/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import addData, Add_Doc, UserData
+
+
+# Register your models here.
+admin.site.register(addData)
+admin.site.register(Add_Doc)
+admin.site.register(UserData)
